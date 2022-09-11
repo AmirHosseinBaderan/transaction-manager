b1fd9c0737028d478b265001efffaf5bc534a751
amir hossin <65053996+Amir-Hossin-pr@users.noreply.github.com>
2022-09-11 23:04:52 +0430

    create delete dialog for items

--- transaction_manager/lib/widgets/home/transactions_list.dart
+++ transaction_manager/lib/widgets/home/transactions_list.dart
@@ -1,45 +1,82 @@
 import 'package:flutter/material.dart';
+import 'package:transaction_manager/constant.dart';
+import 'package:transaction_manager/models/transaction.dart';
+import 'package:transaction_manager/screens/home_screen.dart';
 import 'package:transaction_manager/widgets/home/transaction_item.dart';
 
 class TransactionList extends StatelessWidget {
-  const TransactionList({super.key});
+  List<Transaction> transactions;
+  Function(int itemId) onItemRemoved;
+  TransactionList(
+      {super.key, required this.transactions, required this.onItemRemoved});
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      width: double.infinity,
-      child: Column(children: const [
-        TransactionItem(
-          amount: 1500,
-          date: '1401/06/21',
-          payed: true,
-          title: 'Service',
-        ),
-        TransactionItem(
-          amount: 2000,
-          date: '1401/06/10',
-          payed: false,
-          title: 'Buy',
-        ),
-        TransactionItem(
-          amount: 1500,
-          date: '1401/06/21',
-          payed: true,
-          title: 'Service',
-        ),
-        TransactionItem(
-          amount: 1500,
-          date: '1401/06/21',
-          payed: true,
-          title: 'Service',
-        ),
-        TransactionItem(
-          amount: 2000,
-          date: '1401/06/21',
-          payed: false,
-          title: 'Buy',
-        ),
-      ]),
-    );
+    return Expanded(
+        child: ListView.builder(
+            itemCount: transactions.length,
+            itemBuilder: (context, index) {
+              return GestureDetector(
+                  onLongPress: () {
+                    showDialog(
+                        context: context,
+                        builder: (builder) => AlertDialog(
+                              actionsAlignment: MainAxisAlignment.spaceBetween,
+                              title: const Text(
+                                'حذف تراکنش',
+                                style: TextStyle(fontSize: 14),
+                              ),
+                              icon: const Icon(Icons.delete),
+                              content: Text(
+                                  'آیا از حذف `${HomeScreen.transactions[index].description}` مطمئن هستید؟'),
+                              actions: [
+                                Padding(
+                                    padding: const EdgeInsets.all(5),
+                                    child: ElevatedButton(
+                                        style: ButtonStyle(
+                                            backgroundColor:
+                                                MaterialStateProperty.all(
+                                                    kPrupleColor)),
+                                        onPressed: () {
+                                          onItemRemoved(HomeScreen
+                                              .transactions[index].id);
+                                          Navigator.pop(context);
+                                        },
+                                        child: Row(
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.center,
+                                          children: const [
+                                            Text('بله'),
+                                            Icon(Icons.delete)
+                                          ],
+                                        ))),
+                                Padding(
+                                    padding: const EdgeInsets.all(5),
+                                    child: ElevatedButton(
+                                        style: ButtonStyle(
+                                            backgroundColor:
+                                                MaterialStateProperty.all(
+                                                    kRedColor)),
+                                        onPressed: () {
+                                          Navigator.pop(context);
+                                        },
+                                        child: Row(
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.center,
+                                          children: const [
+                                            Text('خیر'),
+                                            Icon(Icons.cancel)
+                                          ],
+                                        )))
+                              ],
+                            ));
+                  },
+                  child: TransactionItem(
+                    amount: transactions[index].amount,
+                    date: transactions[index].date,
+                    payed: transactions[index].isPayed,
+                    title: transactions[index].description,
+                  ));
+            }));
   }
 }
