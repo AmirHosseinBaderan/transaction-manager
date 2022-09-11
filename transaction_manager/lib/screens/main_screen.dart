b12db98bac954f79ee950411472e7c0f406d2e49
amir hossin <65053996+Amir-Hossin-pr@users.noreply.github.com>
2022-09-11 23:05:14 +0430

    move all widgets to safe area

--- transaction_manager/lib/screens/main_screen.dart
+++ transaction_manager/lib/screens/main_screen.dart
@@ -16,25 +16,27 @@ class _MainScreenState extends State<MainScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      bottomNavigationBar: AnimatedBottomNavigationBar(
-        icons: [Icons.home, Icons.info],
-        activeIndex: currentPage,
-        inactiveColor: Colors.black54,
-        onTap: (index) {
-          if (index == 0) {
-            screen = const HomeScreen();
-          } else {
-            screen = const InfoScreen();
-          }
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Colors.white,
+        bottomNavigationBar: AnimatedBottomNavigationBar(
+          icons: [Icons.home, Icons.info],
+          activeIndex: currentPage,
+          inactiveColor: Colors.black54,
+          onTap: (index) {
+            if (index == 0) {
+              screen = const HomeScreen();
+            } else {
+              screen = const InfoScreen();
+            }
 
-          setState(() {
-            currentPage = index;
-          });
-        },
+            setState(() {
+              currentPage = index;
+            });
+          },
+        ),
+        body: screen,
       ),
-      body: screen,
     );
   }
 }
