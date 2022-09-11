c645175282253cdffbac2b15cb34c186de062b06
amir hossin <65053996+Amir-Hossin-pr@users.noreply.github.com>
2022-09-11 23:06:10 +0430

    get text controller and set it from parent screen

--- transaction_manager/lib/widgets/newTransaction/input_text.dart
+++ transaction_manager/lib/widgets/newTransaction/input_text.dart
@@ -3,15 +3,20 @@ import 'package:flutter/material.dart';
 class InputOutline extends StatelessWidget {
   final String hint;
   final TextInputType inputTtype;
+  final TextEditingController controller;
 
   const InputOutline(
-      {super.key, required this.hint, this.inputTtype = TextInputType.text});
+      {super.key,
+      required this.hint,
+      required this.controller,
+      this.inputTtype = TextInputType.text});
 
   @override
   Widget build(BuildContext context) {
     return Padding(
         padding: const EdgeInsets.all(10),
         child: TextField(
+            controller: controller,
             keyboardType: inputTtype,
             decoration: InputDecoration(
               border: const OutlineInputBorder(
